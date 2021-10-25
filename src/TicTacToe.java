c806a2aea2b5dd5f9b1ba0274eef66d149623553
Soumya795 <soumyasubhakanta97@gmail.com>
2021-10-25 19:35:20 +0530

    Welcome To The Tic Tac Toe Game

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,16 @@
 public class TicTacToe {
+
+    static void createBoard(){
+        char[]board=new char[10];
+        for (int i=1;i<10;i++){
+            board[i]=' ';
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Game of Tic Tac Toe");
+        createBoard();
+
+    }
 }
