860b95fa66b37b0d39f8b40bc18774ef2c2d5aa5
Soumya795 <soumyasubhakanta97@gmail.com>
2021-10-25 20:14:24 +0530

    Show Board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class TicTacToe {
     static char player, computer;
     public static Scanner s = new Scanner(System.in);
+    static char[] board = new char[10];
 
     static void createBoard() {
-        char[] board = new char[10];
+
         for (int i = 1; i < 10; i++) {
             board[i] = ' ';
         }
@@ -22,10 +23,18 @@ public class TicTacToe {
 
     }
 
+    static void showBoard() {
+        System.out.println( board[1] + "--|-" + board[2] + "-|-" + board[3] + "-|--");
+        System.out.println( board[4] + "--|-" + board[5] + "-|-" + board[6] + "-|--");
+        System.out.println( board[7] + "--|-" + board[8] + "-|-" + board[9] + "-|--");
+
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Welcome To The Game of Tic Tac Toe");
         createBoard();
         choice();
+        showBoard();
     }
 }
