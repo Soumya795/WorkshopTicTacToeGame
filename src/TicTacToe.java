c9db5b4c44f2148d03a7d7fc4707b66c91d76990
Soumya795 <soumyasubhakanta97@gmail.com>
2021-10-25 20:37:14 +0530

    User Move

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class TicTacToe {
     static char player, computer;
+    static int location;
     public static Scanner s = new Scanner(System.in);
     static char[] board = new char[10];
 
@@ -18,23 +19,36 @@ public class TicTacToe {
         player = Character.toUpperCase(s.next().charAt(0));
         if (player == 'X')
             computer = 'O';
-        else
+        else if (player == 'O')
             computer = 'X';
+        else
+            System.out.println("Invalid choice");
 
     }
 
     static void showBoard() {
-        System.out.println( board[1] + "--|-" + board[2] + "-|-" + board[3] + "-|--");
-        System.out.println( board[4] + "--|-" + board[5] + "-|-" + board[6] + "-|--");
-        System.out.println( board[7] + "--|-" + board[8] + "-|-" + board[9] + "-|--");
+        System.out.println(board[1] + "--|-" + board[2] + "-|-" + board[3] + "-|--");
+        System.out.println(board[4] + "--|-" + board[5] + "-|-" + board[6] + "-|--");
+        System.out.println(board[7] + "--|-" + board[8] + "-|-" + board[9] + "-|--");
 
     }
 
+    static void userMove() {
+        System.out.println("Enter A position between 1 to 9 ");
+        location = s.nextInt();
+        for (int i = 0; i < location / 2; i++)
+            if (location > 0 && location < 10) {
+                board[location] = player;
+                showBoard();
+            } else
+                System.out.println("Wrong Input Start from the Beginning");
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome To The Game of Tic Tac Toe");
         createBoard();
         choice();
         showBoard();
+        userMove();
     }
 }
