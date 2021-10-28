87f7730a00c3d79e68325091f247320648daac83
Soumya795 <soumyasubhakanta97@gmail.com>
2021-10-28 20:36:56 +0530

    Checking After Every Move

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -5,6 +5,8 @@ public class TicTacToe {
     static int location;
     public static Scanner s = new Scanner(System.in);
     static char[] board = new char[10];
+    private static boolean playerWinToss;
+    private static boolean isWinner=false;
 
     static void toss() {
         double toss = Math.floor(Math.random() * 10) % 2;
@@ -44,15 +46,94 @@ public class TicTacToe {
 
     static void userMove() {
         System.out.println("Enter A position between 1 to 9 ");
-        location = s.nextInt();
 
-        if (location > 0 && location < 10) {
-            board[location] = player;
-            showBoard();
-        } else
-            System.out.println("Wrong Input Start from the Beginning");
+        while (true) {
+            location = s.nextInt();
+            s.nextLine();
+            if (isEmpty(location) && location > 0 && location < 10) {
+                board[location] = player;
+                showBoard();
+                break;
+            } else {
+                System.out.println("Wrong Input Start from the Beginning");
+            }
+
+        }
+
     }
 
+    public static boolean isEmpty(int location) {
+        return board[location] == ' ';
+    }
+    static void computerMove(){
+        System.out.println("Computer Plays");
+        while (!isEmpty(location))
+        {
+            location = (int) Math.floor( Math.random()*((100%9)+1));
+        }
+        board[location]=computer;
+        showBoard();
+
+    }
+
+
+    public static boolean checkBoard() {
+        if ((board[1] != ' ') && (board[2] != ' ') && (board[3] != ' ') && (board[4] != ' ') && (board[5] != ' ')
+                && (board[6] != ' ') && (board[7] != ' ') && (board[8] != ' ') && (board[9] != ' ')) {
+
+            return true;
+        }
+        return false;
+    }
+
+     public static boolean checkWinner() {
+         if (isWinner)
+             return true;
+         if ((board[1] == player && board[2] == player && board[3] == player)
+                 || (board[4] == player && board[5] == player && board[6] == player)
+                 || (board[7] == player && board[8] == player && board[9] == player)
+                 || (board[1] == player && board[4] == player && board[7] == player)
+                 || (board[2] == player && board[5] == player && board[8] == player)
+                 || (board[3] == player && board[6] == player && board[9] == player)
+                 || (board[1] == player && board[5] == player && board[9] == player)
+                 || (board[3] == player && board[5] == player && board[7] == player)) {
+             System.out.println("Player is the WINNER!!");
+             isWinner = true;
+             return true;
+         }
+
+         if ((board[1] == computer && board[2] == computer && board[3] == computer)
+                 || (board[4] == computer && board[5] == computer && board[6] == computer)
+                 || (board[7] == computer && board[8] == computer && board[9] == computer)
+                 || (board[1] == computer && board[4] == computer && board[7] == computer)
+                 || (board[2] == computer && board[5] == computer && board[8] == computer)
+                 || (board[3] == computer && board[6] == computer && board[9] == computer)
+                 || (board[1] == computer && board[5] == computer && board[9] == computer)
+                 || (board[3] == computer && board[5] == computer && board[7] == computer)) {
+             System.out.println("Computer is the WINNER");
+             isWinner = true;
+             return true;
+         }
+         return false;
+     }
+         static void startGame() {
+             while (!checkWinner() && !checkBoard()) {
+                 if (playerWinToss) {
+                     userMove();
+                     if (!checkBoard()) {
+                         computerMove();
+                     } else {
+                         computerMove();
+                         if (!checkBoard())
+                             userMove();
+                     }
+                     if (!checkBoard() && !checkBoard())
+                         System.out.println("Game Tied");
+                 }
+
+
+             }
+         }
 
     public static void main(String[] args) {
         System.out.println("Welcome To The Game of Tic Tac Toe");
@@ -61,6 +142,6 @@ public class TicTacToe {
         choice();
         showBoard();
         userMove();
-
+        startGame();
     }
 }
